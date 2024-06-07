b0d6bb158c69e21028fa59b8c7e94923308d830a
BerkeWest <berkebati@gmail.com>
2024-06-07 03:13:22 +0300

    .

--- alg.py
+++ alg.py
@@ -9,24 +9,17 @@ df = pd.read_excel('data.xlsx')
 
 user= User(user_id,df)
 
-while True:
-    try:
-        id=1
-        if id != user_id:
-            match_user = User(id,df)
-            calculate_similarity(user, match_user)
-            id++
-        elif:
-            id++
-    except KeyError:
-        break
-    except IndexError:
-        break
-
 def calculate_similarity(user, match_user):
-    userAOG = allowsOppositeGender(user)
-    mUserAOG = allowsOppositeGender(match_user)
-    if (userAOG and mUserAOG) or 
+    if (user.allowOppositeGender and match_user.allowOppositeGender) or (user.gender == match_user.gender):
+        print("es")
 
-def allowsOppositeGender(user):
-    return user.allowOppositeGender
+while True:
+    id=1
+    if id != user_id:
+        match_user = User(id,df)
+        calculate_similarity(user, match_user)
+        id = id + 1
+    elif id > 1000:
+        break
+    else:
+        id = id + 1
